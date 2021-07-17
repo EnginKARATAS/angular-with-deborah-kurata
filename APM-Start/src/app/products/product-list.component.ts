5040fe04f32b3a45ef2046ec8524fd78c68db909
Engin Karataş <enginkaratas99@gmail.com>
2021-07-17 21:50:12 +0300

    show - hide image in angular+
    toggle Image functon allows to change show image property. show image property changes with (click) event to true or false. {{showImage ? 'hide' :  'show'}}

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,22 +1,33 @@
 import { Component } from "@angular/core";
 
-@Component({ 
-    selector:"pm-products",
-    templateUrl:"./product-list.component.html"
+@Component({
+    selector: "pm-products",
+    templateUrl: "./product-list.component.html"
 })
 
-export class ProductListComponent{
-    pageTitle:string = "Merhaba Angular";
+export class ProductListComponent {
+    pageTitle: string = "Merhaba Angular";
+    showImage: boolean = true;
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+
     products: any[] = [
         {
-            "productId" : 1,
-            "productName" : "Qualcom Saat",
-            "quantityPerUnit" : 10
+            "productId": 1,
+            "productName": "Qualcom Saat",
+            "quantityPerUnit": 10,
+            "imageUrl": "assets/images/garden_cart.png"
         },
         {
-            "productId" : 2,
-            "productName" : "Casio Saat",
-            "quantityPerUnit" : 14
+            "productId": 2,
+            "productName": "Casio Saat",
+            "quantityPerUnit": 14,
+            "imageUrl": "assets/images/hammer.png"
+
         }
     ]
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }    
 }

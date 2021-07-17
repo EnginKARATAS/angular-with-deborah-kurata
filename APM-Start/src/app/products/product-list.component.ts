e563cfbe0d12491b4c8a7b548b8d56ccf70982de
Engin Karataş <enginkaratas99@gmail.com>
2021-07-17 23:31:22 +0300

    work with angular.

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -6,22 +6,30 @@ import { Component } from "@angular/core";
 })
 
 export class ProductListComponent {
-    pageTitle: string = "Merhaba Angular";
+    pageTitle: string =     "Merhaba Angular";
     showImage: boolean = true;
     imageWidth: number = 50;
     imageMargin: number = 2;
+
+    listFilter: string = "cart";
 
     products: any[] = [
         {
             "productId": 1,
             "productName": "Qualcom Saat",
             "quantityPerUnit": 10,
+            "productCode": "abC",
+            "price": 100,
+            "starRating": 4,
             "imageUrl": "assets/images/garden_cart.png"
         },
         {
             "productId": 2,
             "productName": "Casio Saat",
             "quantityPerUnit": 14,
+            "productCode": "efg",
+            "price": 222,
+            "starRating": 3,
             "imageUrl": "assets/images/hammer.png"
 
         }
